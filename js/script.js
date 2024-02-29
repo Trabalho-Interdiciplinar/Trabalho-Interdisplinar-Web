667720a0d79457000af499571a88f76b0b42cb8a
Leandro Costa <leandrohm.w@gmail.com>
2024-02-29 18:04:05 -0300

    iniciando o login e registro padrao

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let btn_cadastrar = document.querySelector('.btn-register');
+let btn_login = document.querySelector('.btn-login');
+
+let container_login = document.querySelector('.container-login');
+let container_register = document.querySelector('.container-register');
+
+btn_cadastrar.addEventListener('click', function(){
+    container_register.classList.remove('sumir')
+    container_login.classList.add('sumir')
+});
+btn_login.addEventListener('click', function(){
+    container_register.classList.add('sumir')
+    container_login.classList.remove('sumir')
+})
