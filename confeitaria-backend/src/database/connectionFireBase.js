b89f69d096d6c6d0f02834607992018db4d9f10f
Leandro Costa <leandrohm.w@gmail.com>
2024-03-12 18:31:31 -0300

    Add connection js firebase

---
+++ confeitaria-backend/src/database/connectionFireBase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCP0Qk0j77SUKAmd2ORDD7qMg2M1Mp6AuQ",
+  authDomain: "sweetconnection-923af.firebaseapp.com",
+  projectId: "sweetconnection-923af",
+  storageBucket: "sweetconnection-923af.appspot.com",
+  messagingSenderId: "248424880202",
+  appId: "1:248424880202:web:f55c54411c749106cc6eb3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
