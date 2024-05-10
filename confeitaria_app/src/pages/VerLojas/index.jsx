5fc1b5eb94b025790bbb4c494ca7bb6c3f5794ce
Seraffy <gabrielsantos.s2018@outlook.com>
2024-05-10 20:20:59 -0300

    tentando fazeer o retorno das confeitarias

--- confeitaria_app/src/pages/VerLojas/index.jsx
+++ confeitaria_app/src/pages/VerLojas/index.jsx
@@ -1,27 +1,22 @@
 import "./VerLojas.css"
+import { useState, useEffect } from 'react';
 import axios from 'axios'
 
 export function VerLojas (){
 
-    const confeitaria =[ {
-        nome: '',
-        bairro: '',
-        numero: '',
-        logradouro: '',
-        descricao: '',
-        telefone: '',
-        celular: '',
-        logo_url: '',
-    }]
-    axios.get('http://localhost:3001/verlojas').then(response => {
-        console.log(response)
-        confeitaria =reponse.data;
+    const [confeitarias, setConfeitarias] = useState([])
+
+
+        axios.get("http://localhost:3001/verlojas").then((response) => {
+        console.log(response.data)
+        setConfeitarias(response.data);
     })
-    .catch(err => console.log(err))
+       
+
 
     return(
         <div className="verlojas">
-            <h1 className="">HELLO WORLD</h1>
+            <h1>HELLO WORLD</h1>
         </div>
     )
 }
