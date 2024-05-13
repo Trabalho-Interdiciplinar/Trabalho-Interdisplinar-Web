53ce76ce2ece0fa5669e07d6ccdeb90f56c8a6c1
Paulo Augusto Freitas de Sousa <159742970+PauloAFSousa@users.noreply.github.com>
2024-05-12 23:34:43 -0300

    css ladingpage
    Estilização da LadingPage do site

--- confeitaria_app/src/pages/Landing/index.jsx
+++ confeitaria_app/src/pages/Landing/index.jsx
@@ -34,20 +34,6 @@ export function Landing() {
             <img className="deliveryFlow" src="/src/img/flow_lading.png" alt="" />
           </div>
         </main>
-        <div className="navbar">
-          <h1 className="logo">SWEET CONNECTION</h1>
-          <p className="login"><a href="loja/login">LOGIN</a></p>
-          <p className="login"><a href="verlojas">VER LOJAS</a></p>
-        </div>
-        <div className="content">
-          <p>Olá, bem vindo ao nosso sistema Sweet-Connection. 
-            Uma solução criada para micro-empresas com a finalidade de ajula-los a irem mais longe com suas vendas e crescer seu negocio de uma forma mais sistematizada e tecnologica.
-            Se ainda nao faz parte do nosso time de associados, venha se assosiar com o melhor site para confeitarias!
-          </p>
-        </div>
-        <footer>
-
-        </footer>
       </body>
     </>
   );
